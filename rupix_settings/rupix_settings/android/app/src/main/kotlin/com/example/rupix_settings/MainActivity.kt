75fbfbda90466c86d508e032c77ac3bbda684892
Enterprise <82478202+AbrahamRusty@users.noreply.github.com>
2025-09-30 08:40:41 +0700

    merapihkan file & folder

--- rupix_settings/rupix_settings/android/app/src/main/kotlin/com/example/rupix_settings/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.rupix_settings
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- rupix_transfer/android/app/src/main/kotlin/com/example/rupix_transfer/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.rupix_transfer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
